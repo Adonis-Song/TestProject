3dc725a4c3d09994c77be3edd2a4e64f0cb47445
songzhuo <songzhuo@bluepulse.cn>
2021-06-02 19:02:10 +0800

    1

--- app/src/main/java/com/arthenica/mysongapplication/audio/audioedit/MainActivity2.java
+++ app/src/main/java/com/arthenica/mysongapplication/audio/audioedit/MainActivity2.java
@@ -8,11 +8,6 @@ import com.arthenica.mysongapplication.R;
 
 public class MainActivity2 extends AppCompatActivity {
 
-  // Used to load the 'native-lib' library on application startup.
-  static {
-    System.loadLibrary("native-lib");
-  }
-
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
