f73f8c2fb1a0e90cd0d1995b6d3f6e57eafd0f9b
songzhuo <songzhuo@bluepulse.cn>
2021-06-02 19:15:43 +0800

    4

--- app/src/main/java/com/arthenica/mysongapplication/audio/audioedit/service/AudioTaskService.java
+++ app/src/main/java/com/arthenica/mysongapplication/audio/audioedit/service/AudioTaskService.java
@@ -8,27 +8,29 @@ import android.content.Intent;
  */
 public class AudioTaskService extends IntentService {
 
-  private AudioTaskHandler mTaskHandler;
+    private AudioTaskHandler mTaskHandler;
 
-  public AudioTaskService() {
-    super("AudioTaskService");
-  }
-
-  @Override public void onCreate() {
-    super.onCreate();
-
-    mTaskHandler = new AudioTaskHandler();
-  }
-
-  /**
-   * 实现异步任务的方法
-   *
-   * @param intent Activity传递过来的Intent,数据封装在intent中
-   */
-  @Override protected void onHandleIntent(Intent intent) {
-
-    if (mTaskHandler != null) {
-      mTaskHandler.handleIntent(intent);
+    public AudioTaskService() {
+        super("AudioTaskService");
     }
-  }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mTaskHandler = new AudioTaskHandler();
+    }
+
+    /**
+     * 实现异步任务的方法
+     *
+     * @param intent Activity传递过来的Intent,数据封装在intent中
+     */
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        if (mTaskHandler != null) {
+            mTaskHandler.handleIntent(intent);
+        }
+    }
 }
